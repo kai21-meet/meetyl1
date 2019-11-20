00e5eadda45abc4ac375fc82e3420461e39a2dc9
kai21-meet <52973235+kai21-meet@users.noreply.github.com>
2019-11-20 20:05:26 +0200

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -13,14 +13,35 @@ class user(object):
 		self.friend_list.remove(email)
 		print(self.name + " has removed " + email + " as a friend")
 	
-	def post(self, text):
-		self.posts.append(text)
-		print(self.name + " has posted " + text)
+	def posting(self, post_text):
+		self.post_text = post_text
+		post1=Post(post_text)
+		self.posts.append(post1)
+		print(self.name + " has posted " + post_text)
 
 	def get_userInfo(self):
-		print ("Name:" + [self.name] / "Email:" + [self.email] / "Password:" + [self.password] / "Friends:" +str[self.friends_list] / "Posts:" + str[self.posts])
+		print("Name:" + self.name)
+		print("Email:" + self.email)
+		print("Password:" + self.password)sers=[]
+
+		print("Friends:" +str(self.friend_list))
+		print("Posts:" + str(self.posts))
+
+
+class Post():
+	def __init__(self,post_text):
+		self.comments = []
+		self.post_text = post_text
+
 
 user1 = user("kai","kai.assraf@gmail.com","kaiassraf123")
 user2 = user("lani","lani.assraf@gmail.com","laniassraf123")
 user1.add_friend('lani')
-user1.post("heyy how r u?")
+user1.posting("heyy how r u?")
+user1.get_userInfo()
+user1.remove_friend('lani')
+
+
+
+
+		
