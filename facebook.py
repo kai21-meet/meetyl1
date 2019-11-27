7dc0c995fcb81e0db5f32e71270b9f402196f3c0
kai21-meet <52973235+kai21-meet@users.noreply.github.com>
2019-11-27 18:44:20 +0200

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -1,10 +1,13 @@
-class user(object):
+users=[]
+posts_list=[]
+
+class User(object):
 	def __init__ (self, name, email, password):
 		self.name = name
 		self.email = email
 		self.password = password
 		self.friend_list=[]
-		self.posts=[]
+
 	def add_friend(self, email):
 		self.friend_list.append(email)
 		print(self.name + " has added " + email + " as a friend")
@@ -13,35 +16,61 @@ class user(object):
 		self.friend_list.remove(email)
 		print(self.name + " has removed " + email + " as a friend")
 	
-	def posting(self, post_text):
-		self.post_text = post_text
-		post1=Post(post_text)
-		self.posts.append(post1)
-		print(self.name + " has posted " + post_text)
+	def posting(self, text):
+		post1=Post(0, text, self.email)
+		posts_list.append(post1)
+		print(self.name + " has posted " + text)
 
 	def get_userInfo(self):
 		print("Name:" + self.name)
 		print("Email:" + self.email)
-		print("Password:" + self.password)sers=[]
-
+		print("Password:" + self.password)
 		print("Friends:" +str(self.friend_list))
-		print("Posts:" + str(self.posts))
 
 
 class Post():
-	def __init__(self,post_text):
-		self.comments = []
-		self.post_text = post_text
+	def __init__(self,text,likes,author):
+		self.text=text
+		self.likes=0
+		self.comments=[]
+		self.author=author
+
+	def like_post (self, email):
+		self.like=+1
+		print(email+ " liked your post ")
+
+	def comment_post(self, text):
+		self.comments.append(text)
+		print(email+ " commented your post " + text)
+
+class Comment(Post):
+	def __init__(self, like):
+		self.like = 0
+		self.reply=[]
+
+	def like_comments(self, likes):
+		self.like=+1
+		print(email+ " liked your comment ")
+
+	def reply_to(self, text):
+		self.reply.append(text)
+		print(email+ " replyed your comment " + text)
 
 
-user1 = user("kai","kai.assraf@gmail.com","kaiassraf123")
-user2 = user("lani","lani.assraf@gmail.com","laniassraf123")
+'''
+user1 = User("kai","kai.assraf@gmail.com","kaiassraf123")
+user2 = User("lani","lani.assraf@gmail.com","laniassraf123")
 user1.add_friend('lani')
 user1.posting("heyy how r u?")
 user1.get_userInfo()
 user1.remove_friend('lani')
+'''
 
+print("log in")
+email=input('your email')	
+password=input('your password')
+for user in users():
+	if email==self.email:
+		if password==self.password:
+			log_in=user
 
-
-
-		
